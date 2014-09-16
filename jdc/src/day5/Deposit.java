fd31a6e41b119b7b6c155676891fe3abe4301076
vsniko <vsniko@gmail.com>
2014-09-16 18:55:09 +0300

    Deposit task

--- jdc/src/day5/Deposit.java
+++ jdc/src/day5/Deposit.java
@@ -3,16 +3,13 @@ package day5;
 public class Deposit {
 
 	public static void main(String[] args) {
-		System.out.println(darkZone(100, 12, 1000000));
+		System.out.println(darkZone(500, 12, 1000000));
 	}
 		public static String darkZone(int monthlyPayment, int interest, int dreamSum) {
 			int balance = 0;
-			double percent;
 			int month = 0;
 			while(balance < dreamSum) {
-				balance += monthlyPayment;
-				percent = balance * (interest / 1200);
-				balance += percent;
+				balance += monthlyPayment + balance * ((interest / 12) / 100);
 				month++;
 			}
 			return month / 12 + " years and " + month % 12 + " months";
